8db62cfa834a0e516827545699640d589c31e07b
dhruvind11 <dhruvindobariya811@gmail.com>
2026-03-29 14:19:56 +0530

    fix: ensure GOOGLE_CLIENT_ID defaults to an empty string if not set

--- frontend/components/GoogleOAuthWrapper.tsx
+++ frontend/components/GoogleOAuthWrapper.tsx
@@ -3,8 +3,7 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import type { ReactNode } from "react";
 
-console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
-const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||'';
 
 export default function GoogleOAuthWrapper({
   children,
